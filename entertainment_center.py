cd94e71e405307b20bef59cd7f68d7dd01416d92
Tiffany <tiffany.c.stallings@gmail.com>
2017-07-22 09:59:19 -0400

    Add init to Movie class and create an instance of movie called little_shop to house movie info.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,11 @@
 import media
 
-little_shop = media.Movie()
+little_shop = media.Movie("Little Shop of Horrors",
+                          "A nerdy florist finds his chance " +
+                          "for success and romance with the " +
+                          "help of a giant man-eating plant " +
+                          "who demands to be fed.",
+                          "https://upload.wikimedia.org/wikipedia/en/5/5b/Little_shop_of_horrors.jpg",
+                          "https://www.youtube.com/watch?v=jFENSU8CmZk")
+
+print(little_shop.storyline)
