8d07b4ee6c986b346022985f5608780202172b47
Tiffany <tiffany.c.stallings@gmail.com>
2017-07-22 10:17:11 -0400

    Add new movie instance to entertainment_center

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,4 +8,9 @@ little_shop = media.Movie("Little Shop of Horrors",
                           "https://upload.wikimedia.org/wikipedia/en/5/5b/Little_shop_of_horrors.jpg",
                           "https://www.youtube.com/watch?v=jFENSU8CmZk")
 
-print(little_shop.storyline)
+phantom_opera = media.Movie("Phantom of the Opera",
+                            "A mysterious 'Angel of Music " +
+                            "controls the goings-on of an " +
+                            "1870's opera house.",
+                            "https://upload.wikimedia.org/wikipedia/en/d/d6/Poto2.jpg",
+                            "https://www.youtube.com/watch?v=N91AL8sAh9o")
