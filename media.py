8e4f34ac8e4bdf7776a588d58582eca282741fcb
Tiffany <tiffany.c.stallings@gmail.com>
2017-07-25 16:55:43 -0400

    Fix tabs in  media.py to be consistent

--- media.py
+++ media.py
@@ -6,25 +6,24 @@ class Movie():
     A template that holds information on movies and allows the
     user to play the movie's trailer.
     '''
-    
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-    	'''
-    	Takes inputs of the movie's title (string), 
-    	storyline (string), poster image url (string), and 
-    	youtube trailer url (string) and outputs an instance 
-    	of Movie that holds these values.
-    	'''
+        '''
+        Takes inputs of the movie's title (string), 
+	storyline (string), poster image url (string), and 
+	youtube trailer url (string) and outputs an instance 
+	of Movie that holds these values.
+	'''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-    	'''
-    	Takes no inputs and produces no outputs. On running
-    	instance.show_trailer(), the program will open a window
-    	in the user's preferred browser that plays the youtube
-    	trailer stored in the object by __init__.
-    	'''
+        '''
+        Takes no inputs and produces no outputs. On running
+	instance.show_trailer(), the program will open a window
+	in the user's preferred browser that plays the youtube
+	trailer stored in the object by __init__.
+	'''
         webbrowser.open(self.trailer_youtube_url)
